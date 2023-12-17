7092cfb9829c7cbbce359c65ffad8ff6530152f2
sudhanshu-kumar81 <sudhanshukumar8181@gmail.com>
2023-12-17 15:11:35 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,35 @@
+
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+function App() {
+  
+  const[count,setcount]=useState(0);
+  function decreaseHandler(){
+      setcount(count-1);
+
+  }
+  function increaseHandler(){
+    setcount(count+1);
+
+}
+function resetHandler(){
+  setcount(0);
+
+}
+  return (
+  <div className=" w-[100vw] h-[100vh] bg-gray-200 flex justify-center items-center flex-col gap-y-10 ">
+    <div className=" text-[#0398d4] font-medium text-2xl"> increment && decrement </div>
+    <div  className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+        <button onClick={decreaseHandler} class=" border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">-</button>
+       <div className="font-bold gap-12 text-5xl ">  {count}   </div>
+       <button onClick={increaseHandler} class=" border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">+</button>
+     </div>
+    <div > 
+    <button onClick={resetHandler} className='bg-[#0398d4]  text-white px-5 py-2 rounded-sm text-lg'>reset</button>     
+     </div>
+  </div>
+  );
+}
+
+export default App;
